a515f2dbb75a0409a08ea9e7459ba61f28c0a232
Brunys <brunys@brunysixl.xyz>
2024-12-03 18:12:07 -0500

    BARE FIX FINAL

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/ixl/',
-    bare:'https://bare.lostlogs.org/', /* Thanks benrogo */
+    bare:'https://static.99.135.156.178.clients.your-server.de/', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
